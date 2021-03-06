3da6037dfa73fcc6bea8bc61affb5f07224396a6
Kirstin Holsman <kholsman@kkh-computer.local>
2021-03-05 18:31:07 -0800

    fixed exit directory paths

--- R/getZip.R
+++ R/getZip.R
@@ -1,6 +1,18 @@
-getZip <- function(url,destfile, deletezip=T){
+getZip <- function(url,destfile,deletezip=F){
   download.file(url=url, destfile=destfile)
-  unzip (destfile, exdir = "./",overwrite = T)
+  tmp_wd <- getwd()
+  
+  tt     <- strsplit(destfile,"/")
+  ex_dir <- substr(destfile,1,nchar(destfile)-4)
+  tt     <- tt[[1]][length(tt[[1]])]
+  
+  
+  if(!dir.exists(ex_dir)) dir.create(ex_dir)
+  #setwd(substr(tt,1,nchar(tt)-4))
+  unzip(destfile,exdir = ex_dir,overwrite = T)
+  
   if(deletezip)
     file.remove(destfile)
+  setwd(tmp_wd)
+  
 }
