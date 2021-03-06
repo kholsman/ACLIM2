dd5303e8a91bee6651d7fce4429b6d442e6d8208
Kirstin Holsman <kholsman@kkh-computer.local>
2021-03-05 19:00:32 -0800

    fixed directory path for getZip

--- R/getZip.R
+++ R/getZip.R
@@ -1,18 +1,24 @@
 getZip <- function(url,destfile,deletezip=F){
+  
+  # download the file
   download.file(url=url, destfile=destfile)
   tmp_wd <- getwd()
-  
+  # get root directory
   tt     <- strsplit(destfile,"/")
   ex_dir <- substr(destfile,1,nchar(destfile)-4)
   tt     <- tt[[1]][length(tt[[1]])]
   
-  
+  # create directory
   if(!dir.exists(ex_dir)) dir.create(ex_dir)
-  #setwd(substr(tt,1,nchar(tt)-4))
-  unzip(destfile,exdir = ex_dir,overwrite = T)
+  setwd(ex_dir)
   
+  # unzip it
+  unzip(zipfile=file.path(tmp_wd,destfile),exdir = ".",overwrite = T)
+  
+  setwd(tmp_wd)
+  # cleanup
   if(deletezip)
     file.remove(destfile)
-  setwd(tmp_wd)
+  
   
 }
