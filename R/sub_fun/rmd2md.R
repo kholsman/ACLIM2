667249e18f24a4ee2f6cb2d31b73ebfdcca1cb78
Kirstin Holsman <kholsman@kkh-computer.local>
2021-03-07 14:00:52 -0800

    README updated

--- R/sub_fun/rmd2md.R
+++ R/sub_fun/rmd2md.R
@@ -1,5 +1,5 @@
 ## convert markdown doc to read me file for github
-rmd2md <- function(rmd_fl = "README_Holsman_EBMpaper",md_fl = "README"){
+rmd2md <- function(rmd_fl,md_fl){
   library(rmarkdown)
   render(paste0(rmd_fl,".Rmd"), md_document(variant = "markdown_github"),params=F)
   file.copy(from=paste0(rmd_fl,".md"),to=paste0(md_fl,".md"),overwrite=T)
